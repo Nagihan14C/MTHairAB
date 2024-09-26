a04b742783433edfe2e1a82a50ff3697ac1c34c9
Nagihan14C <143505890+Nagihan14C@users.noreply.github.com>
2024-09-26 14:10:36 +0200

    vet ej

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import './css/grid.css';
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/footer';
+import Header from './components/header';
+import { AppRouter } from './router/approuter';
+
+function App() {
+  return (
+    <BrowserRouter basename="/MTHairAB"> {/* Lägg till basename här */}
+      <Header />
+      <AppRouter />
+      <Footer />
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+
+/* Exercise
+1. Add component <Musicalbums/> from 08-app-bs-async as a page component
+2. Setup NavMenu and AppRouter so that a navigation alternative Music navigates to page Musicalbums
+  - remember to also compy Services
+3. Very that it works
+
+4. Add <AnimalPage10a/> from M4L3/01-building-form-components as a page component
+5. Setup NavMenu and AppRouter so that a navigation alternative Animals navigates to page <AnimalPage10a/>
+   - remember copy move the Model and Services
+
+6. Very that it works
+*/
--- src/router/approuter.js
+++ src/router/approuter.js
@@ -8,10 +8,19 @@ import Kontakt1 from '../pages/kontakt1';
 export function AppRouter() {
   return (
     <Routes>
+<<<<<<< HEAD
       <Route path="/" element={<Home />} />
       <Route path="/tjanster" element={<Tjanster />} />
       <Route path="/kontakt1" element={<Kontakt1 />} />
       <Route path="/about" element={<About />} />
+=======
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/tjanster' element={<Tjanster/>}></Route>
+        <Route path='/kontakt1' element={<Kontakt1/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+
+
+>>>>>>> d133998f69ba5ec3f3d1dddce8eb7799a4f79a4e
     </Routes>
   );
 }
