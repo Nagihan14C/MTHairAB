26695acf70be97990b6e33eb445226ac8e7715af
Nagihan14C <143505890+Nagihan14C@users.noreply.github.com>
2024-09-26 16:31:45 +0200

    merge grej

--- router/approuter.js
+++ router/approuter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
+
 import Home from '../pages/home';
 import About from '../pages/about';
 import Tjanster from '../pages/tjanster';
@@ -15,4 +16,3 @@ export function AppRouter() {
     </Routes>
   );
 }
-
